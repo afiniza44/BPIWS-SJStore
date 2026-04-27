72d912d55978b66799d5f4c5adb58d5ac415b767
finciouss <muhammadafinizza1@gmail.com>
2026-04-27 07:35:42 +0700

    fix: trust proxies for Railway HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         \App\Providers\NeonServiceProvider::class,
     ])
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminOnly::class,
         ]);
